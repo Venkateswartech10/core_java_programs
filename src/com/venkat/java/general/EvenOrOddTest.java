3e2a1c621a535fd83ed2daeef60b0eb6f5739656
Venkateswartech10 <venkateswar.tech10@gmail.com>
2026-02-06 11:02:34 +0530

    Test class added

---
+++ src/com/venkat/java/general/EvenOrOddTest.java
@@ -0,0 +1,14 @@
+package com.venkat.java.general;
+
+public class EvenOrOddTest {
+
+	public static void main(String[] args) {
+		int num = 6;
+		if(num % 2 == 0) {
+			System.out.println("Given number is Even");
+		}else {
+			System.out.println("Given number is Odd");
+		}
+	}
+
+}
