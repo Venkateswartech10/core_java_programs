725c419ad6bc78f9de2180075d731a7478070435
Venkateswartech10 <venkateswar.tech10@gmail.com>
2026-03-28 16:59:56 +0530

    Pattern progrm added

---
+++ src/com/venkat/java/general/patterns/PatternProgram1.java
@@ -0,0 +1,24 @@
+package com.venkat.java.general.patterns;
+
+/*
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * *   
+*/
+public class PatternProgram1 {
+
+	public static void main(String[] args) {
+		pattern1(5);
+	}
+
+	private static void pattern1(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
