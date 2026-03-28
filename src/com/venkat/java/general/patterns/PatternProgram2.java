5045347b33d8dba27b7099f9f2e4d2336a5a2960
Venkateswartech10 <venkateswar.tech10@gmail.com>
2026-03-28 17:10:46 +0530

    Pattern program2 added

---
+++ src/com/venkat/java/general/patterns/PatternProgram2.java
@@ -0,0 +1,29 @@
+package com.venkat.java.general.patterns;
+
+/*
+*  
+* * 
+* * *  
+* * * * 
+* * * * *   
+*/
+
+/*1.For the outer loop, count the no of lines.
+2.For the inner loop, focus on the columns & connect them somehow to the rows.
+3.Print them "*" inside the inner for loop.
+4.Observe Symmetry ( Optional ). */
+public class PatternProgram2 {
+
+	public static void main(String[] args) {
+		pattern(5);
+	}
+
+	private static void pattern(int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
