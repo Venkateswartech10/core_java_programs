d75911ca8dfe6639a74ba1b393cdcb6457a9a1ff
Venkateswartech10 <venkateswar.tech10@gmail.com>
2026-04-12 22:13:21 +0530

    Sum of first numbers program modified

--- src/com/venkat/java/general/recursion/NumberSumTest.java
+++ src/com/venkat/java/general/recursion/NumberSumTest.java
@@ -7,13 +7,24 @@ public class NumberSumTest {
 		int num = 5;
 		// Pass the first param as the number and second param as default sum
 		printSum(num, 0);
+		// Pass the given number
+		int sum = printSumByFunctional(num);
+		System.out.println(sum);
+	}
+
+	private static int printSumByFunctional(int num) {
+		if (num == 0) {
+			return 0;
+		}
+		return num + printSumByFunctional(num - 1);
+
 	}
 
 	private static void printSum(int num, int sum) {
 		// Base condition to break the loop
 		if (num < 1) {
 			// Print the sum when ever iteration comes to Zero and retun
-			System.out.print(sum);
+			System.out.println(sum);
 			return;
 		}
 		// Call the recursion function by decrementing number by 1
