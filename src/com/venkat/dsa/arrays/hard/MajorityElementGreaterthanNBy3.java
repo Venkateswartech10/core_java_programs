f3835dc3b5c0adc3e85a46d85920025e0baac0a4
Venkateswartech10 <venkateswar.tech10@gmail.com>
2026-06-28 12:21:53 +0530

    Program renamed

--- src/com/venkat/dsa/arrays/hard/MajorityElementGreaterthanNBy3.java
+++ src/com/venkat/dsa/arrays/hard/MajorityElementGreaterthanNBy3.java
@@ -11,7 +11,7 @@ public class MajorityElementGreaterthanNBy3 {
 		int[] arr = {11, 33, 33, 11, 33, 11};
 		// Time Complexity: O(N^2), where N is the size of the array. This is because for each element, we are traversing the entire array to count its occurrences.
 		// Space Complexity: O(1), as we are using a constant amount of space for the result array, which can hold at most 2 elements.
-		List<Integer> ans = majorityElementTwoBrute(arr);
+		List<Integer> ans = majorityElementThreeBrute(arr);
 
         System.out.print("The majority elements are: ");
         for (int it : ans) {
@@ -21,7 +21,7 @@ public class MajorityElementGreaterthanNBy3 {
         
         // Time Complexity: O(N * logN), where N is the size of the given array. For using a map data structure, where insertion in the map takes logN time, and we are doing it for N elements. So, it results in the first term O(N * logN). On using unordered_map instead, the first term will be O(N) for the best and average case, and for the worst case, it will be O(N2).
         // Space Complexity: O(N) for using a map data structure. A list that stores a maximum of 2 elements is also used, but that space used is so small that it can be considered constant.
-        List<Integer> ans1 = majorityElementTwoBetter(arr);
+        List<Integer> ans1 = majorityElementThreeBetter(arr);
         System.out.print("The majority elements are: ");
         for (int it : ans1) {
             System.out.print(it + " ");
@@ -30,7 +30,7 @@ public class MajorityElementGreaterthanNBy3 {
         
         // Time Complexity: O(N), where N is the size of the input array. We traverse the array twice: once to find potential candidates and once to validate them.
         // Space Complexity: O(1), as we are using a constant amount of space for the counters and candidate elements, regardless of the input size.
-        List<Integer> ans2 = majorityElementTwoOptimal(arr);
+        List<Integer> ans2 = majorityElementThreeOptimal(arr);
         System.out.print("The majority elements are: ");
         for (int it : ans2) {
             System.out.print(it + " ");
@@ -40,11 +40,11 @@ public class MajorityElementGreaterthanNBy3 {
 	}
 
 	// Function to find majority elements in an array
-	private static List<Integer> majorityElementTwoOptimal(int[] nums) {
+	private static List<Integer> majorityElementThreeOptimal(int[] nums) {
 		int n = nums.length;
 		int cnt1 = 0, cnt2 = 0;
 		int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) 	{
 			if (cnt1 == 0 && el2 != nums[i]) {
 				cnt1 = 1;
 				el1 = nums[i];
@@ -81,7 +81,7 @@ public class MajorityElementGreaterthanNBy3 {
 	}
 
 	// Function to find majority elements in an array
-	private static List<Integer> majorityElementTwoBetter(int[] nums) {
+	private static List<Integer> majorityElementThreeBetter(int[] nums) {
 		int n = nums.length;
 		List<Integer> result = new ArrayList<>();
 		Map<Integer, Integer> mpp = new HashMap<>();
@@ -101,7 +101,7 @@ public class MajorityElementGreaterthanNBy3 {
 	}
 
 	// Function to find majority elements in an array
-	private static List<Integer> majorityElementTwoBrute(int[] arr) {
+	private static List<Integer> majorityElementThreeBrute(int[] arr) {
 		int n = arr.length;
 		List<Integer> result = new ArrayList<>();
 
